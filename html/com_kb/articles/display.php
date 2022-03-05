f67ebfd065375ad91bdb0d2e5eb2e5fa2ebd712f
M. Drew LaMar <mdlama@wm.edu>
2022-03-05 17:31:37 -0500

    [override] route kb page to support page
    (cherry picked from commit 81359d300b583dcee0d4a9c37043baecae86822b)

---
+++ html/com_kb/articles/display.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright (c) 2005-2020 The Regents of the University of California.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+// No direct access.
+defined('_HZEXEC_') or die();
+
+// Reroute to support page
+header("Location: " . Route::url('index.php?option=com_support'));
+exit();
+?>
